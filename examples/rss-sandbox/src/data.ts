ba351b6dc12d61de6164d449806f2f2a407545d6
Pranav HS <pranavprati@gmail.com>
2025-08-30 14:10:17 +0530

    > scroll left & right

--- examples/rss-sandbox/src/data.ts
+++ examples/rss-sandbox/src/data.ts
@@ -44,84 +44,86 @@ export const data = [
         "image": "https://picsum.photos/400/309",
         "caption": "A shot of food"
     },
-    {
-        "id": 10,
-        "image": "https://picsum.photos/400/310",
-        "caption": "A picture of animals"
-    },
-    {
-        "id": 11,
-        "image": "https://picsum.photos/400/311",
-        "caption": "A tranquil forest scene"
-    },
-    {
-        "id": 12,
-        "image": "https://picsum.photos/400/312",
-        "caption": "A vibrant street art piece"
-    },
-    {
-        "id": 13,
-        "image": "https://picsum.photos/400/313",
-        "caption": "A desert landscape at dusk"
-    },
-    {
-        "id": 14,
-        "image": "https://picsum.photos/400/314",
-        "caption": "An extreme close-up"
-    },
-    {
-        "id": 15,
-        "image": "https://picsum.photos/400/315",
-        "caption": "A photo of an event"
-    },
-    {
-        "id": 16,
-        "image": "https://picsum.photos/400/316",
-        "caption": "A black and white image"
-    },
-    {
-        "id": 17,
-        "image": "https://picsum.photos/400/317",
-        "caption": "A picture of a building interior"
-    },
-    {
-        "id": 18,
-        "image": "https://picsum.photos/400/318",
-        "caption": "A shot of a car"
-    },
-    {
-        "id": 19,
-        "image": "https://picsum.photos/400/319",
-        "caption": "An aerial view"
-    },
-    {
-        "id": 20,
-        "image": "https://picsum.photos/400/320",
-        "caption": "A picture of the sky"
-    },
-    {
-        "id": 21,
-        "image": "https://picsum.photos/400/321",
-        "caption": "A shot of a boat"
-    },
-    {
-        "id": 22,
-        "image": "https://picsum.photos/400/322",
-        "caption": "A photo of a bridge"
-    },
-    {
-        "id": 23,
-        "image": "https://picsum.photos/400/323",
-        "caption": "A picture of a flower"
-    },
-    {
-        "id": 24,
-        "image": "https://picsum.photos/400/324",
-        "caption": "A wide-angle landscape"
-    },
-    {
-        "id": 25,
-        "image": "https://picsum.photos/400/325",
-        "caption": "A photo of a mountain"
-    }
 ]
+
+
+// {
+//     "id": 10,
+//     "image": "https://picsum.photos/400/310",
+//     "caption": "A picture of animals"
+// },
+// {
+//     "id": 11,
+//     "image": "https://picsum.photos/400/311",
+//     "caption": "A tranquil forest scene"
+// },
+// {
+//     "id": 12,
+//     "image": "https://picsum.photos/400/312",
+//     "caption": "A vibrant street art piece"
+// },
+// {
+//     "id": 13,
+//     "image": "https://picsum.photos/400/313",
+//     "caption": "A desert landscape at dusk"
+// },
+// {
+//     "id": 14,
+//     "image": "https://picsum.photos/400/314",
+//     "caption": "An extreme close-up"
+// },
+// {
+//     "id": 15,
+//     "image": "https://picsum.photos/400/315",
+//     "caption": "A photo of an event"
+// },
+// {
+//     "id": 16,
+//     "image": "https://picsum.photos/400/316",
+//     "caption": "A black and white image"
+// },
+// {
+//     "id": 17,
+//     "image": "https://picsum.photos/400/317",
+//     "caption": "A picture of a building interior"
+// },
+// {
+//     "id": 18,
+//     "image": "https://picsum.photos/400/318",
+//     "caption": "A shot of a car"
+// },
+// {
+//     "id": 19,
+//     "image": "https://picsum.photos/400/319",
+//     "caption": "An aerial view"
+// },
+// {
+//     "id": 20,
+//     "image": "https://picsum.photos/400/320",
+//     "caption": "A picture of the sky"
+// },
+// {
+//     "id": 21,
+//     "image": "https://picsum.photos/400/321",
+//     "caption": "A shot of a boat"
+// },
+// {
+//     "id": 22,
+//     "image": "https://picsum.photos/400/322",
+//     "caption": "A photo of a bridge"
+// },
+// {
+//     "id": 23,
+//     "image": "https://picsum.photos/400/323",
+//     "caption": "A picture of a flower"
+// },
+// {
+//     "id": 24,
+//     "image": "https://picsum.photos/400/324",
+//     "caption": "A wide-angle landscape"
+// },
+// {
+//     "id": 25,
+//     "image": "https://picsum.photos/400/325",
+//     "caption": "A photo of a mountain"
+// }
--- packages/react-snap-slider/src/components/slider/Slider.tsx
+++ packages/react-snap-slider/src/components/slider/Slider.tsx
@@ -1,15 +1,13 @@
 import * as React from 'react'
-import styles from './slider.module.css';
 import { useWindowDimensions } from '../../hooks/use-window-dimension.hook';
-import { calculateMargin } from '../../utils/calulate-margin.util';
 import { useAutoMargin } from '../../hooks/use-auto-margin';
 
 const Slider: React.FC<any> = ({ children, settings }) => {
     const minGap = settings?.minGap ?? 10;
     const sliderContainer = React.useRef(null);
     const containerRef = React.useRef(null);
-    const [s, ss] = React.useState(0);
-    const [cardWidth, setCardWidth] = React.useState(55);
+    const [cardWidth, setCardWidth] = React.useState(0);
+    const [sliderOffset, setSliderOffset] = React.useState(0);
     const { width: windowWidth } = useWindowDimensions();
     const gap = useAutoMargin({ containerRef: sliderContainer, cardWidth, minGap, deps: [windowWidth] });
 
@@ -19,21 +17,31 @@ const Slider: React.FC<any> = ({ children, settings }) => {
             setCardWidth(firstChild.offsetWidth);
         }
     }, []);
-
+    function navigate(direction: 'l' | 'r', scrollAmount = cardWidth + gap) {
+        let updatedSliderOffset = 0;
+        if (containerRef.current) {
+            if (direction === 'r') {
+                updatedSliderOffset = Math.min(containerRef.current.offsetWidth - sliderContainer.current.offsetWidth, sliderOffset + scrollAmount);
+            } else {
+                updatedSliderOffset = Math.max(0, sliderOffset - scrollAmount);
+            }
+        }
+        setSliderOffset(updatedSliderOffset);
+    }
     React.useEffect(() => {
 
     }, []);
     return (
         <>
             <div className="slider-container" ref={sliderContainer}>
-                <button role="button" aria-label="Previous slide" className="navigation navigation-left" >
+                <button role="button" aria-label="Previous slide" className="navigation navigation-left" onClick={() => { navigate('l') }}>
                     <div className="arrow left" ></div>
                 </button>
-                <div style={{ display: 'flex', gap: gap + 'px', padding: `0 ${gap / 2}px` }} ref={containerRef}>
+                <div style={{ display: 'flex', gap: gap + 'px', padding: `0 ${gap / 2}px`, transform: `translateX(${-sliderOffset}px)`, transition: 'transform 0.4s ease-in-out' }} ref={containerRef}>
                     {children}
                 </div>
 
-                <button role="button" aria-label="Next slide" className="navigation navigation-right">
+                <button role="button" aria-label="Next slide" className="navigation navigation-right" onClick={() => { navigate('r') }}>
                     <div className="arrow right"></div>
                 </button>
             </div>
